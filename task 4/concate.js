683722e0140eeeba203e75d2724a7e8881223e1d
Gowda0511 <shankaresh.t.n2017@kgkite.ac.in>
2020-08-28 00:04:20 +0530

    Update concate.js

--- task 4/concate.js
+++ task 4/concate.js
@@ -1,4 +1,4 @@
 var sen=" Hello world !  ";
-var result = sen.split(" ").filter((item) => item != "");
-var res = result[0].concat(" ",result[result.length-1]);
-console.log(res);
+var result = sen.split(" ").filter((item) => item != ""); // string to array with whitespace
+var res = result[0].concat(" ",result[result.length-1]);  // concat first and last
+console.log(res); // Hello !
